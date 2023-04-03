d474cedced903ab117fb647acf638ffaad2ac73a
EihabAhmed <eihabahmed@gmail.com>
2023-04-03 07:55:33 +0200

    Added tabs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,32 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: '',
+    redirectTo: 'tabs',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'tabs',
+    loadComponent: () => import('./pages/tabs/tabs.page').then( m => m.TabsPage)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/tabs/home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'search',
+    loadComponent: () => import('./pages/tabs/search/search.page').then( m => m.SearchPage)
+  },
+  {
+    path: 'orders',
+    loadComponent: () => import('./pages/tabs/orders/orders.page').then( m => m.OrdersPage)
+  },
+  {
+    path: 'wallet',
+    loadComponent: () => import('./pages/tabs/wallet/wallet.page').then( m => m.WalletPage)
+  },
+  {
+    path: 'account',
+    loadComponent: () => import('./pages/tabs/account/account.page').then( m => m.AccountPage)
   },
 ];
